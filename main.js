facc3ad6f1c74cb1f2a9852595ff0ea0f5287898
Matheus Afonso <matheusafonsouza@gmail.com>
2024-09-15 19:19:55 -0300

    comentarios

--- main.js
+++ main.js
@@ -8,17 +8,20 @@ import {
   servers,
 } from './constants';
 
+// instância do firebase
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 const firestore = firebase.firestore();
 
+// instância da comunicação RTC e variáveis globais
 const pc = new RTCPeerConnection(servers);
 let localStream = null;
 let remoteStream = null;
 let videoEnabled = true;
 let audioEnabled = true;
 
+// todos os compontentes utilizados do HTML
 const webcamVideo = document.getElementById('webcamVideo');
 const callButton = document.getElementById('callButton');
 const callInput = document.getElementById('callInput');
@@ -30,6 +33,8 @@ const chatList = document.getElementById('chatList');
 const videoButton = document.getElementById('videoButton');
 const micButton = document.getElementById('micButton');
 
+// responsável por iniciar a camera junto com o audio e video (que é configurado pelos botões)
+// além disso, também já instancia o envio dos tracks para a comunicação RTC
 const startWebcam = async () => {
   localStream = await navigator.mediaDevices.getUserMedia({ video: videoEnabled, audio: audioEnabled });
   remoteStream = new MediaStream();
@@ -51,8 +56,10 @@ const startWebcam = async () => {
   answerButton.disabled = false;
 }
 
+// responsável por recuperar uma chamada dado um id
 const getOrCreateCall = (id) => firestore.collection('calls').doc(id);
 
+// responsável por adicionar uma mensagem ao firebase e limpar o input de chat
 chatInputButton.onclick = async () => {
   const callDoc = getOrCreateCall(callInput.value);
   callDoc.collection('chat').add({ message: chatInput.value });
@@ -64,6 +71,8 @@ callButton.onclick = async () => {
 
   callInput.value = callDoc.id;
 
+  // essa parte do código instancia o callback necessário para registrar os candidatos offer
+  // dentro do firebase
   pc.onicecandidate = (event) => {
     event.candidate && callDoc.collection('offerCandidates').add(event.candidate.toJSON());
   };
@@ -78,6 +87,7 @@ callButton.onclick = async () => {
 
   await callDoc.set({ offer });
 
+  // essa parte do código instancia a remote description para a sessão RTC
   callDoc.onSnapshot((snapshot) => {
     const data = snapshot.data();
     if (!pc.currentRemoteDescription && data?.answer) {
@@ -85,6 +95,7 @@ callButton.onclick = async () => {
     }
   });
 
+  // essa parte do código recupera os answerCandidates para uma chamada
   callDoc.collection('answerCandidates').onSnapshot((snapshot) => {
     snapshot.docChanges().forEach((change) => {
       if (change.type === 'added') {
@@ -93,6 +104,7 @@ callButton.onclick = async () => {
     });
   });
 
+  // essa parte do código recupera as mensagens do chat para uma chamada específica
   callDoc.collection('chat').onSnapshot((snapshot) => {
     snapshot.docChanges().forEach((change) => {
       if (change.type === 'added') {
@@ -110,6 +122,8 @@ callButton.onclick = async () => {
 answerButton.onclick = async () => {
   const callDoc = getOrCreateCall(callInput.value);
 
+  // essa parte do código instancia o callback necessário para registrar os candidatos answer
+  // dentro do firebase
   pc.onicecandidate = (event) => {
     event.candidate && callDoc.collection('answerCandidates').add(event.candidate.toJSON());
   };
@@ -127,6 +141,7 @@ answerButton.onclick = async () => {
   };
   await callDoc.update({ answer });
 
+  // essa parte do código recupera os offerCandidates para uma chamada
   callDoc.collection('offerCandidates').onSnapshot((snapshot) => {
     snapshot.docChanges().forEach((change) => {
       if (change.type === 'added') {
@@ -135,6 +150,7 @@ answerButton.onclick = async () => {
     });
   });
 
+  // essa parte do código recupera as mensagens do chat para uma chamada específica
   callDoc.collection('chat').onSnapshot((snapshot) => {
     snapshot.docChanges().forEach((change) => {
       if (change.type === 'added') {
@@ -149,12 +165,14 @@ answerButton.onclick = async () => {
   chatInputButton.disabled = false;
 };
 
+// responsável pelo botão de desligar o vídeo
 videoButton.onclick = async () => {
   videoEnabled = !videoEnabled;
   const videoStream = localStream.getVideoTracks()[0];
   videoStream.enabled = videoEnabled;
 }
 
+// responsável pelo botão de desligar o audio
 micButton.onclick = async () => {
   audioEnabled = !audioEnabled;
   const audioStream = localStream.getAudioTracks()[0];
